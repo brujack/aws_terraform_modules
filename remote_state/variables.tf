aa14525f9fc6b0d31512369b13c84d2d2380a0b5
Bruce Jackson <bjackson@pobox.com>
2019-06-04 18:32:50 -0500

    removed unused variables

--- remote_state/variables.tf
+++ remote_state/variables.tf
@@ -2,15 +2,3 @@ variable "terraform_bucket_name" {
   description = "The name of the s3 bucket where terraform.tfstate will be stored"
   default     = "conecrazy-test-terraform-remote-state-storage-s3"
 }
-
-variable "remote_state_full_access_users" {
-  description = "User list of users with full r/w access to the remote state file"
-  type        = any
-  #default     = []
-}
-
-variable "remote_state_read_users" {
-  description = "User list of users with read only access to the remote state file"
-  type        = list(string)
-  #default     = []
-}
